f1c31a572988b14259872b38f4394689617a07af
GrantC515 <111007253+GrantC515@users.noreply.github.com>
2023-05-01 11:26:32 -0700

    Scripts started and UI updated

---
+++ Final Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] public float _moveSpeed = 10f;
+    public float _charHealth = 5f;
+    public TextMeshProUGUI healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        GameOver();
+    }
+
+    void Movement()
+    {
+        float xValue = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, -zValue, 0f);
+    }
+
+    public void UpdateHealth()
+    {
+        _charHealth --;
+        healthText.text = _charHealth.ToString();
+    }
+
+        private void OnCollisionEnter(Collision other) 
+    {
+       if(other.gameObject.CompareTag("Obstacle")) 
+        {
+            UpdateHealth();
+        }
+    }
+
+    public void GameOver()
+    {
+        if(_charHealth <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
---
+++ Final Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadStartScene()
+    {
+        //SceneManager.LoadScene("Course #1");
+    }
+
+    public void ReplayFromGO()
+    {
+       // SceneManager.LoadScene("Course #1");
+    }
+
+    public void ReplayFromWin()
+    {
+       // SceneManager.LoadScene("Course #1");
+    }
+}
